f51279ffc65151363868eb8db8e5a61dc42f2a7d
Dan <DanielWRoberts@outlook.com>
2017-07-20 07:27:11 -0700

    Did initial examples with require.
    Finished video Section 3: 10 "Requiring Your Own Files"

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 "use strict";
 
-console.log('Starting App...');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
 
-let user = os.userInfo().username;
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', `Hello ${user}!`, (err) => {
-  if (err) {
-    console.log('Unable to write to the file');
-  }
-});
+// let user = os.userInfo();
 
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
+//   if (err) {
+//     console.log('Unable to write to the file');
+//   }
+// });
 
+// let res = notes.addNote();
+// console.log(res);
+
+console.log(`5 + 3 is ${notes.add(5, 3)}`);
 
