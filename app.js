6779ad6eae7961f5ea68515093e01af3623cadb4
Dan <DanielWRoberts@outlook.com>
2017-07-20 09:43:47 -0700

    Installed and Learnt nodemon
    Finished video Section 3: 12 "Restarting App with Nodemon"

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 "use strict";
 
-console.log('Starting app.js');
-
 const fs = require('fs');
-const os = require('os');
-
 const _ = require('lodash');
 
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('Dan'));
-
-let filteredArray = _.uniq(['Dan', 1, 'Dan', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// let user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
-//   if (err) {
-//     console.log('Unable to write to the file');
-//   }
-// });
-
-// let res = notes.addNote();
-// console.log(res);
-
-// console.log(`5 + 3 is ${notes.add(5, 3)}`);
 
--- notes.js
+++ notes.js
@@ -1,13 +1,7 @@
 "use strict";
 
-console.log('Starting notes.js');
-
 module.exports.addNote = function() {
   console.log('addNote');
   return 'New note';
 };
-
-module.exports.add = function(a, b) {
-  return a + b;
-}
 
