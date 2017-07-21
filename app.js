a7431f14919a2902ee9116f0e79fab6512af789a
Dan <DanielWRoberts@outlook.com>
2017-07-21 06:41:39 -0700

    Added code to read command line arguments
    The app.js file now responds differently based upon what argument is
    passed when running the application.
    
    Finished video Section 3: 13 "Getting Input From User"

--- app.js
+++ app.js
@@ -5,4 +5,30 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+let command = process.argv[2];
+
+console.log('Command: ' + command);
+console.log(process.argv);
+
+switch(command) {
+  case "add":
+    console.log("Adding new note...");
+    break;
+  case "list":
+    console.log("Listing all notes...");
+    break;
+  case "read":
+    console.log("Reading note...");
+    break;
+  case "remove":
+    console.log("Removing note...");
+    break;
+  case undefined:
+  case "":
+  case " ":
+    console.log("Command not found");
+    break;
+  default:
+    console.log("Command not recognized");
+}
 
