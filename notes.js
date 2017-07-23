2b1037db0a29ca5b28b54460e9783252a116bd51
Dan <DanielWRoberts@outlook.com>
2017-07-23 14:44:06 -0700

    Added the ability to save notes with unique titles
    This was done with fs and saved the files to an array of JSONs in a
    file called "notes-data.json".
    Finished video section 3: 16 "Adding and Saving Notes".

--- notes.js
+++ notes.js
@@ -1,7 +1,34 @@
 "use strict";
 
+const fs = require('fs');
+
 let addNote = (title, body) => {
-  console.log("Adding note...", title, body);
+  let notes = [];
+  let note = {
+    title,
+    body
+  };
+
+  try {
+    let notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+    // notes already has a default value
+  }
+
+  let duplicateNotes = notes.filter((note) => {
+    return note.title === title;
+  });
+
+  if(duplicateNotes.length < 1) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    // console.log("Note Was Successfully Added");
+  } else {
+    // there was already a note with that title
+    // console.log("Note Was A Duplicate!");
+  }
+
 };
 
 let getAll = () => {
